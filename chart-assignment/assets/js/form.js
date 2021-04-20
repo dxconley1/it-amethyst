14953f1a7da5450eb1fb232b99ec297059716aa1
DanellConley <ec2-user@ip-172-31-24-201.ec2.internal>
2021-04-20 17:36:19 +0000

    value

---
+++ chart-assignment/assets/js/form.js
@@ -0,0 +1,120 @@
+function GetStock() {
+    "use strict";
+
+    // Get a reference to the form - Use the ID of the form
+    var form = $("#myform");
+    
+    // Validate all of the for elements
+    form.validate();
+    
+    // If all of the form elements are valid, the get the form values
+    if (form.valid()) {
+        
+        var StockSymbol = document.getElementById("StockSymbol").value;
+        var apiKey = "35eaVfKsObXpSg2O4kMLj9udr2DgVW1f"
+        var FromDate = document.getElementById("FromDate").value;
+        var ToDate = document.getElementById("ToDate").value;
+
+        
+        /* URL for AJAX Call */
+        var myURL2 = "https://api.polygon.io/v2/aggs/ticker/" + StockSymbol + "/range/1/day/" + FromDate + "/" + ToDate + "?unadjusted=false&sort=asc&limit=32&apiKey=" + apiKey;
+        myURL2 = "https://api.polygon.io/v2/aggs/ticker/C:EURUSD/range/1/day/2020-10-14/2020-10-20?unadjusted=true&sort=asc&limit=120&apiKey=35eaVfKsObXpSg2O4kMLj9udr2DgVW1f";
+
+        /* AJAX Method (POST or GET) */
+        var myMethod2 = "GET";
+
+        /* Make sure the document is ready */
+        $(document).ready(function() { 
+
+            /* Perform AJAX call - Note that the AJAX function 
+               does not have a selector */
+
+            $.ajax({
+              method: myMethod2,
+              url: myURL2
+            })
+
+            /* AJAX complete - result is in msg */
+            .done(function( msg2 ) {
+
+                /* Your code to process the result goes here  
+                    display the returned message */
+                var stockdate = [];
+                var stockvalue = [];
+                var stockvolume = [];
+                var numdays = msg2.results.length;
+                if (numdays > 0) {
+                    for (var i = 0; i < numdays; i++) {
+                        /* stock close value */
+                        stockvalue[i] = msg2.results[i].c;
+                       
+                        /* date is in Unix milleseconds - create a temporary date variable */
+                        var tempdate = new Date(msg2.results[i].t);
+                        /* extract the date string from the value */
+                        stockdate[i] = tempdate.toLocaleDateString();
+                    }
+                }
+
+                var stockvaluetable = "";
+                if (numdays > 0) {
+                    stockvaluetable = stockvaluetable + "<table><caption>Stock Price</caption><tr><th>Date</th><th>Price</th></tr>";
+                    for (var i = 0; i < numdays; i++) {
+                        stockvaluetable = stockvaluetable + "<tr><td>" + stockdate[i] + "</td><td>" + stockvalue[i] + "</td></tr>";
+                    }
+                    stockvaluetable = stockvaluetable + "</table>"
+                    document.getElementById("StockValueTable").innerHTML = stockvaluetable;
+                }
+                
+               
+
+                var ctx0 = document.getElementById("chartjs-0");
+                var myChart = new Chart(ctx0, {
+                    "type":"line",
+                    "data": {
+                        "labels": stockdate,
+                        "datasets":[{"label":"Stock Close",
+                        "data": stockvalue,
+                        "fill":false,
+                        "borderColor":"rgb(75, 192, 192)",
+                        "lineTension":0.1}]},
+                        "options":{ 
+                            responsive: false,
+                            maintainAspectRatio: true,
+                        }
+                    }
+                );
+            })
+            
+            /* AJAX complete with error - probably invalid stock ticker symbol */
+            .fail(function( msg ) {
+
+                /* Your code to process the result goes here - 
+                   display the returned message */
+                alert("Stock2 Not Found - Status: " + msg.status)
+            });
+        });
+    }
+}
+
+function ClearForm() {
+    document.getElementById("StockSymbol").value = "";
+    document.getElementById("FromDate").value = "";
+    document.getElementById("ToDate").value = "";
+    document.getElementById("company").innerHTML = "";
+    document.getElementById("address").innerHTML = "";
+    document.getElementById("employees").innerHTML = "";
+    document.getElementById("ceo").innerHTML = "";
+    document.getElementById("url").innerHTML = "";
+    document.getElementById("url").href = "";
+    document.getElementById("logo").src = "";
+    document.getElementById("StockValueTable").innerHTML = "";
+    document.getElementById("StockVolumeTable").innerHTML = "";
+    
+    /* Ugly Code to Erase Canvas */
+    var canvas0 = document.getElementById("chartjs-0");
+    var context0 = canvas0.getContext('2d');    
+    context0.clearRect(0, 0, canvas0.width, canvas0.height);
+    var canvas1 = document.getElementById("chartjs-1");
+    var context1 = canvas1.getContext('2d');    
+    context1.clearRect(0, 0, canvas1.width, canvas1.height);
+}
--- cycle/assets/js/form.js
+++ cycle/assets/js/form.js
@@ -1,20 +1,8 @@
 
 $(document).ready(function() {
-    $('.gallery').cycle({
-		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-	});
-	
-	$('.quotes').cycle({
-		 fx:      'fade', 
-		 speed:    2500, 
-		 timeout:  5000 
-	});
-	
-	$('ul.ticker').cycle({
-		 fx:      'scrollHorz', 
-		 speed:    2500, 
-		 timeout:  5000 
-	});
-	
+
+$('.gallery').cycle({
+fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
 });
 
+});
