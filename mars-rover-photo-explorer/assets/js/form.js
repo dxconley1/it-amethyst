0956351af9fe95eef633abbb521f07edec56cd95
DanellConley <ec2-user@ip-172-31-24-201.ec2.internal>
2021-05-06 17:42:55 +0000

    update

--- mars-rover-photo-explorer/assets/js/form.js
+++ mars-rover-photo-explorer/assets/js/form.js
@@ -1,5 +1,86 @@
-for (i = 0; i &lt; 25; i++) {
-    // Note how we construct the name for image1, image2, etc...this sets the image source
-    document.getElementById("image" + i).src = msg.photos[i].img_src;
-    do something to set the tool tip = msg.photos[i].camera.full_name;
+function findrover() {
+    "use strict";
+
+    var form = $("#myform");
+    
+    form.validate();
+    
+    if (form.valid()) {
+        
+        var PictureDate = document.getElementById("PictureDate").value;
+        var apiKey = "OdjHbVhznvBXkD9Jfj3V1Cs42nvqebh4tgM2gO1P";
+        
+        var Rover;
+        if (document.getElementById("Curiosity").checked) {
+            Rover = document.getElementById("Curiosity").value;
+        }
+        if (document.getElementById("Opportunity").checked) {
+            Rover = document.getElementById("Opportunity").value;
+        }
+        if (document.getElementById("Spirit").checked) {
+            Rover = document.getElementById("Spirit").value;
+        }
+       
+        var myURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + Rover + "/photos?earth_date=" + PictureDate + "&page=1&api_key=" + apiKey;
+
+        var myMethod = "GET";
+
+        $(document).ready(function() { 
+
+            $.ajax({
+              method: myMethod,
+              url: myURL
+            })
+            
+            .done(function( msg ) {
+
+                var numpictures = msg.photos.length;
+                if (numpictures > 0) {
+                    for (var i = 0; i < 25; i++) {
+                        if (i < numpictures) {
+                        // Note how we construct the name for image1, image2, etc...this sets the image source
+                            document.getElementById("image" + i).src = msg.photos[i].img_src;
+                            document.getElementById("anchor" + i).href = msg.photos[i].img_src;
+                        //do something to set the tool tip = msg.photos[i].camera.full_name;
+                            document.getElementById("image" + i).title = msg.photos[i].camera.full_name;
+                            document.getElementById("text1").innerHTML = msg.photos.length + " photos found";
+                            document.getElementById("text2").innerHTML = "Click a photo to display full size";
+                        }
+                        
+                        else
+                        {
+                            document.getElementById("image" + i).src = "#";
+						    document.getElementById("anchor" + i).href = "#";
+						    document.getElementById("image" + i).style.display = "none";
+                        }
+                    }
+                }
+            })
+            
+            .fail(function( msg ) {
+                alert("Rover Not Found - Status: " + msg.status);
+            });
+        });
+    }
+}
+
+function clearform() {
+    document.getElementById("Curiosity").checked = false;
+    document.getElementById("Opportunity").checked = false;
+    document.getElementById("Spirit").checked = false;
+    document.getElementById("PictureDate").value = "";
+    document.getElementById("RoverError").innerHTML = "";
+    document.getElementById("PictureDateError").innerHTML = "";
+}
+
+function getCuriosity() {
+    document.getElementById("PictureDate").value = "2012-08-06";
+}
+
+function getOpportunity() {
+    document.getElementById("PictureDate").value = "2004-01-26";
+}
+
+function getSpirit() {
+    document.getElementById("PictureDate").value = "2004-01-05";
 }
