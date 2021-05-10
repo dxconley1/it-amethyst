c08cccc66fe7741072811d812d2064a6d3982600
DanellConley <ec2-user@ip-172-31-24-201.ec2.internal>
2021-05-10 20:34:56 +0000

    update

--- chart-assignment/assets/js/form.js
+++ chart-assignment/assets/js/form.js
@@ -1,81 +1,59 @@
-function GetStock() {
+function calculate() {
     "use strict";
 
-    // Get a reference to the form - Use the ID of the form
     var form = $("#myform");
     
-    // Validate all of the for elements
     form.validate();
     
-    // If all of the form elements are valid, the get the form values
     if (form.valid()) {
         
-        var StockSymbol = document.getElementById("StockSymbol").value;
-        var apiKey = "35eaVfKsObXpSg2O4kMLj9udr2DgVW1f"
+        var BaseCurrency = document.getElementById("BaseCurrency").value;
+        var Convert = document.getElementById("Convert").value;
+        var apiKey = "4OKcDcbFyhgTfATKTCEp8P_NVGzjVc3t";
         var FromDate = document.getElementById("FromDate").value;
         var ToDate = document.getElementById("ToDate").value;
 
-        
-        /* URL for AJAX Call */
-        var myURL2 = "https://api.polygon.io/v2/aggs/ticker/" + StockSymbol + "/range/1/day/" + FromDate + "/" + ToDate + "?unadjusted=false&sort=asc&limit=32&apiKey=" + apiKey;
-        myURL2 = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-10-14/2020-10-14?unadjusted=true&sort=asc&limit=120&apiKey=72iRTb0Kpusy7NtwZ4JkhBHvImLgBn0b
+        var myURL = "https://api.polygon.io/v2/aggs/ticker/C:" + BaseCurrency + Convert + "/range/1/day/" + FromDate + "/" + ToDate + "?unadjusted=true&sort=asc&limit=120&apiKey=" + apiKey;
 
-;
+        var myMethod = "GET";
 
-        /* AJAX Method (POST or GET) */
-        var myMethod2 = "GET";
-
-        /* Make sure the document is ready */
         $(document).ready(function() { 
 
-            /* Perform AJAX call - Note that the AJAX function 
-               does not have a selector */
-
             $.ajax({
-              method: myMethod2,
-              url: myURL2
+              method: myMethod,
+              url: myURL
             })
+            
+            .done(function( msg ) {
 
-            /* AJAX complete - result is in msg */
-            .done(function( msg2 ) {
-
-                /* Your code to process the result goes here  
-                    display the returned message */
-                var stockdate = [];
-                var stockvalue = [];
-                var stockvolume = [];
-                var numdays = msg2.results.length;
+                var currencydate = [];
+                var currencyvalue = [];
+                var numdays = msg.results.length;
                 if (numdays > 0) {
                     for (var i = 0; i < numdays; i++) {
-                        /* stock close value */
-                        stockvalue[i] = msg2.results[i].c;
-                       
-                        /* date is in Unix milleseconds - create a temporary date variable */
-                        var tempdate = new Date(msg2.results[i].t);
-                        /* extract the date string from the value */
-                        stockdate[i] = tempdate.toLocaleDateString();
+                        currencyvalue[i] = msg.results[i].c;
+                        var tempdate = new Date(msg.results[i].t);
+                        currencydate[i] = tempdate.toLocaleDateString();
                     }
                 }
 
-                var stockvaluetable = "";
+                var currencyvaluetable = "";
                 if (numdays > 0) {
-                    stockvaluetable = stockvaluetable + "<table><caption>Stock Price</caption><tr><th>Date</th><th>Price</th></tr>";
+                    currencyvaluetable = currencyvaluetable + "<table><caption>Currency Value</caption><tr><th>Date</th><th>Price</th></tr>";
                     for (var i = 0; i < numdays; i++) {
-                        stockvaluetable = stockvaluetable + "<tr><td>" + stockdate[i] + "</td><td>" + stockvalue[i] + "</td></tr>";
+                        currencyvaluetable = currencyvaluetable + "<tr><td>" + currencydate[i] + "</td><td>" + currencyvalue[i] + "</td></tr>";
                     }
-                    stockvaluetable = stockvaluetable + "</table>"
-                    document.getElementById("StockValueTable").innerHTML = stockvaluetable;
+                    currencyvaluetable = currencyvaluetable + "</table>";
+                    document.getElementById("CurrencyValueTable").innerHTML = currencyvaluetable;
                 }
-                
-               
 
                 var ctx0 = document.getElementById("chartjs-0");
                 var myChart = new Chart(ctx0, {
                     "type":"line",
                     "data": {
-                        "labels": stockdate,
-                        "datasets":[{"label":"Stock Close",
-                        "data": stockvalue,
+                        "labels": currencydate,
+                        "datasets":[{"label":"Currency Value",
+                        "data": currencyvalue,
                         "fill":false,
                         "borderColor":"rgb(75, 192, 192)",
                         "lineTension":0.1}]},
@@ -87,32 +65,21 @@ function GetStock() {
                 );
             })
             
-            /* AJAX complete with error - probably invalid stock ticker symbol */
             .fail(function( msg ) {
 
-                /* Your code to process the result goes here - 
-                   display the returned message */
-                alert("Stock2 Not Found - Status: " + msg.status)
+                alert("Currency Not Found - Status: " + msg.status);
             });
         });
     }
 }
 
-function ClearForm() {
-    document.getElementById("StockSymbol").value = "";
+function clearform() {
+    document.getElementById("BaseCurrency").value = "";
+    document.getElementById("Convert").value = "";
     document.getElementById("FromDate").value = "";
     document.getElementById("ToDate").value = "";
-    document.getElementById("company").innerHTML = "";
-    document.getElementById("address").innerHTML = "";
-    document.getElementById("employees").innerHTML = "";
-    document.getElementById("ceo").innerHTML = "";
-    document.getElementById("url").innerHTML = "";
-    document.getElementById("url").href = "";
-    document.getElementById("logo").src = "";
-    document.getElementById("StockValueTable").innerHTML = "";
-    document.getElementById("StockVolumeTable").innerHTML = "";
+    document.getElementById("CurrencyValueTable").innerHTML = "";
     
-    /* Ugly Code to Erase Canvas */
     var canvas0 = document.getElementById("chartjs-0");
     var context0 = canvas0.getContext('2d');    
     context0.clearRect(0, 0, canvas0.width, canvas0.height);
