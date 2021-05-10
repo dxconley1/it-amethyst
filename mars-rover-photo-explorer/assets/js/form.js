0abadf9c0de5e66eb28d461fcee44f49f6dc9364
DanellConley <ec2-user@ip-172-31-24-201.ec2.internal>
2021-05-10 00:18:43 +0000

    update

--- mars-rover-photo-explorer/assets/js/form.js
+++ mars-rover-photo-explorer/assets/js/form.js
@@ -1,5 +1,5 @@
 function findrover() {
-    "use strict";
+    "use from";
 
     var form = $("#myform");
     
--- unitconversion/assets/js/form.js
+++ unitconversion/assets/js/form.js
@@ -1,64 +1,66 @@
 function calculate() {
     "use strict";
-   
-    if ($( "#myform" ).valid()) {
-       
-        var FromValue = document.getElementById("FromValue").value;
-       
-        var FromUnit;
-        if (document.getElementById("cm").checked) {
-            FromUnit = document.getElementById("cm").value;
+
+    var form = $( "#myform" );
+    
+    if (form.valid()) {
+        
+        var fromvalue = document.getElementById("FromValue").value;
+
+        var fromunit;
+        if (document.getElementById("FCentimeters").checked) {
+            fromunit = document.getElementById("FCentimeters").value;
         }
-        if (document.getElementById("m").checked) {
-            FromUnit = document.getElementById("m").value;
+        if (document.getElementById("FMeters").checked) {
+            fromunit = document.getElementById("FMeters").value;
         }
-        if (document.getElementById("km").checked) {
-            FromUnit = document.getElementById("km").value;
+        if (document.getElementById("FKilometers").checked) {
+            fromunit = document.getElementById("FKilometers").value;
         }
-        if (document.getElementById("in").checked) {
-            FromUnit = document.getElementById("in").value;
+        if (document.getElementById("FInches").checked) {
+            fromunit = document.getElementById("FInches").value;
         }
-        if (document.getElementById("ft").checked) {
-            FromUnit = document.getElementById("ft").value;
+        if (document.getElementById("FFeet").checked) {
+            fromunit = document.getElementById("FFeet").value;
         }
-        if (document.getElementById("yd").checked) {
-            FromUnit = document.getElementById("yd").value;
+        if (document.getElementById("FYards").checked) {
+            fromunit = document.getElementById("FYards").value;
         }
-        if (document.getElementById("mi").checked) {
-            FromUnit = document.getElementById("mi").value;
+        if (document.getElementById("FMiles").checked) {
+            fromunit = document.getElementById("FMiles").value;
         }
-       
-        var ToUnit;
-        if (document.getElementById("tocm").checked) {
-            ToUnit = document.getElementById("tocm").value;
+        
+        var tounit;
+        if (document.getElementById("TCentimeters").checked) {
+            tounit = document.getElementById("TCentimeters").value;
         }
-        if (document.getElementById("tom").checked) {
-            ToUnit = document.getElementById("tom").value;
+        if (document.getElementById("TMeters").checked) {
+            tounit = document.getElementById("TMeters").value;
         }
-        if (document.getElementById("tokm").checked) {
-            ToUnit = document.getElementById("tokm").value;
+        if (document.getElementById("TKilometers").checked) {
+            tounit = document.getElementById("TKilometers").value;
         }
-        if (document.getElementById("toin").checked) {
-            ToUnit = document.getElementById("toin").value;
+        if (document.getElementById("TInches").checked) {
+            tounit = document.getElementById("TInches").value;
         }
-        if (document.getElementById("toft").checked) {
-            ToUnit = document.getElementById("toft").value;
+        if (document.getElementById("TFeet").checked) {
+            tounit = document.getElementById("TFeet").value;
         }
-        if (document.getElementById("toyd").checked) {
-            ToUnit = document.getElementById("toyd").value;
+        if (document.getElementById("TYards").checked) {
+            tounit = document.getElementById("TYards").value;
         }
-        if (document.getElementById("tomi").checked) {
-            ToUnit = document.getElementById("tomi").value;
+        if (document.getElementById("TMiles").checked) {
+            tounit = document.getElementById("TMiles").value;
         }
-       
-        var myURL = "https://brucebauer.info/assets/ITEC3650/unitsconversion.php"
-        var myMethod = "POST"
-       
+        
+        var myURL = "https://brucebauer.info/assets/ITEC3650/unitsconversion.php";
+        var myMethod = "POST";
+
         var myData = {};
-        myData.FromValue = FromValue;
-        myData.FromUnit = FromUnit;
-        myData.ToUnit = ToUnit;
-       
+        myData.FromValue = fromvalue;
+        myData.FromUnit = fromunit;
+        myData.ToUnit = tounit;
+        
         $(document).ready(function() {
 
             $.ajax({
@@ -68,42 +70,36 @@ function calculate() {
             })
 
             .done(function( msg ) {
-                document.getElementById("Result").innerHTML = msg;
+                document.getElementById("ToValue").innerHTML = msg;
             });
         });
-       
     }
 }
 
 function clearform() {
-   
+    "use strict";
+    
     document.getElementById("FromValue").value = "";
     document.getElementById("FromValueError").innerHTML = "";
-    document.getElementById("cm").checked = false;
-    document.getElementById("m").checked = false;
-    document.getElementById("km").checked = false;
-    document.getElementById("in").checked = false;
-    document.getElementById("ft").checked = false;
-    document.getElementById("yd").checked = false;
-    document.getElementById("mi").checked = false;
+    document.getElementById("FCentimeters").checked = false;
+    document.getElementById("FMeters").checked = false;
+    document.getElementById("FKilometers").checked = false;
+    document.getElementById("FInches").checked = false;
+    document.getElementById("FFeet").checked = false;
+    document.getElementById("FYards").checked = false;
+    document.getElementById("FMiles").checked = false;
     document.getElementById("FromUnitError").innerHTML = "";
-    document.getElementById("tocm").checked = false;
-    document.getElementById("tom").checked = false;
-    document.getElementById("tokm").checked = false;
-    document.getElementById("toin").checked = false;
-    document.getElementById("toft").checked = false;
-    document.getElementById("toyd").checked = false;
-    document.getElementById("tomi").checked = false;
+    document.getElementById("TCentimeters").checked = false;
+    document.getElementById("TMeters").checked = false;
+    document.getElementById("TKilometers").checked = false;
+    document.getElementById("TInches").checked = false;
+    document.getElementById("TFeet").checked = false;
+    document.getElementById("TYards").checked = false;
+    document.getElementById("TMiles").checked = false;
     document.getElementById("ToUnitError").innerHTML = "";
-    document.getElementById("Result").innerHTML = "";
+    document.getElementById("ToValue").innerHTML = "";
 }
 
 $( "#myform" ).validate({
- 
-});
- 
 
-/* Form Validation */
-$( "#myform" ).validate({
- 
 });
